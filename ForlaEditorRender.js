d26bf8a980b6d69e1d81b6e4e5579a73e8bcb2ec
Kaibro <jrkaibro@gmail.com>
2020-06-04 20:34:11 -0300

    sending full controler

---
+++ ForlaEditorRender.js
@@ -0,0 +1,60 @@
+function ForlaEditor($)
+{
+	this.Width;
+	this.Height;
+	this.Attribute;
+	this.Theme;
+	this.Toolbar;
+
+	// Databinding for property Attribute
+	this.SetAttribute = function(data)
+	{
+		///UserCodeRegionStart:[SetAttribute] (do not remove this comment.)
+		
+		this.Attribute = data;
+		
+		///UserCodeRegionEnd: (do not remove this comment.)
+	}
+
+	// Databinding for property Attribute
+	this.GetAttribute = function()
+	{
+		///UserCodeRegionStart:[GetAttribute] (do not remove this comment.)
+		
+		return this.Attribute;
+		
+		///UserCodeRegionEnd: (do not remove this comment.)
+	}
+
+	this.show = function()
+	{
+		///UserCodeRegionStart:[show] (do not remove this comment.)
+
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		///UserCodeRegionEnd: (do not remove this comment.)
+	}
+	///UserCodeRegionStart:[User Functions] (do not remove this comment.)
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	///UserCodeRegionEnd: (do not remove this comment.):
+}
---
+++ js/init.js
@@ -0,0 +1,3 @@
+$(function(){
+    $('#FROALAEDITORContainer_textArea').froalaEditor();
+});
